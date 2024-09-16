2956dd99afba621238ccd7738a30043eadfe04ca
Sione <dev.sionejoseph@gmail.com>
2024-09-16 16:54:20 -0500

    api call successful

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 require('dotenv').config();
 
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+const apiKey = process.env.API_KEY
+//console.log(apiKey);
+
+async function getImages(query) {
+    const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}&limit=25&offset=0&rating=g&lang=en`);
+    const parsed = await response.json()
+    
+    console.log(await parsed.data[0].images.original.url) // testing to be sure I'm pulling the correct data and cnavigating it properly.
+}
+
+getImages()
